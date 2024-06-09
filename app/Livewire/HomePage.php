44d5867bb43b88b3efde9c81388313baa081e423
abdallah <boodykhattab97@gmail.com>
2024-06-09 15:24:21 +0300

    home page,category + brands

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,28 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+
+#[Title('Home Page - Abecommerce')]
 class HomePage extends Component
 {
     public function render()
     {
-        return view('livewire.home-page');
+       // $brands = Brand::all();
+
+       // if is active value = 1
+       //display it
+       $brands = Brand::where('is_active',1)->get();
+       $categories = Category::where('is_active',1)->get();
+
+      // dd($brands);
+        return view('livewire.home-page',[
+            'brands' => $brands,
+            'categories' => $categories
+        ]);
     }
 }
