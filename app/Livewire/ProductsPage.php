e6dcf1d68d2cb6a0fa0f3d539b5055197edc2968
abdallah <boodykhattab97@gmail.com>
2024-06-13 19:42:09 +0300

    cart navbar

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 use App\Models\Brand;
 use App\Models\Product;
 use Livewire\Component;
@@ -35,6 +37,13 @@ class ProductsPage extends Component
     public $sort = 'latest';
 
 
+    // add product to cart method
+
+    public function addToCart($product_id){
+      $total_count = CartManagement::addItemToCart($product_id);
+
+      $this->dispatch('update-cart-count',$total_count)->to( Navbar::class);
+    }
 
  
    
