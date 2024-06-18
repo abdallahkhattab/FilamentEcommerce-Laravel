040dd63249daf4225c55481bc522e21f8eb49ffb
abdallah <boodykhattab97@gmail.com>
2024-06-18 14:14:21 +0300

    login and register customer

--- app/Livewire/Auth/LoginPage.php
+++ app/Livewire/Auth/LoginPage.php
@@ -3,9 +3,30 @@
 namespace App\Livewire\Auth;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class LoginPage extends Component
 {
+
+    public $email;
+    public $password;
+
+    protected $rules = [
+        'email' => 'required|email',
+        'password' => 'required|min:8',
+    ];
+
+    public function login()
+    {
+        $this->validate();
+
+        if (Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
+            return redirect()->intended('/dashboard');
+        } else {
+            session()->flash('error', 'Invalid email or password');
+        }
+    }
+    
     public function render()
     {
         return view('livewire.auth.login-page');
--- app/Livewire/Auth/RegisterPage.php
+++ app/Livewire/Auth/RegisterPage.php
@@ -2,10 +2,46 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use Illuminate\Support\Facades\Hash;
 
+
+#[Title("Register")]
 class RegisterPage extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+
+    protected $rules = [
+        'name' => 'required|string|max:255',
+        'email' => 'required|email|unique:users,email',
+        'password' => 'required|string|min:8',
+    ];
+
+    public function submit()
+    {
+        $this->validate();
+
+      $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+
+        session()->flash('success', 'Account created successfully!');
+
+        auth()->login($user);
+
+        //return redirect()->route('login');
+
+        return redirect()->intended();
+    }
+
+
+
     public function render()
     {
         return view('livewire.auth.register-page');
